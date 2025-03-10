6e3687a8c23d17200c65290e6794a3c2ea962f46
Daniel Rajakumar <danielelvisr@gmail.com>
2025-03-10 19:55:03 -0400

    sync

--- OPL/C++/Source Files/Player.cpp
+++ OPL/C++/Source Files/Player.cpp
@@ -12,37 +12,48 @@ Player::Player(Board& b, const bool human) : board(b), isHuman(human) {}
 // Roll dice
 int Player::rollDie() const {
     int diceSum = 0;
+    char choice, isManual;
+    int choiceOne, choiceTwo;
 
-    if (canThrowOneDie()) {
-        if (isHuman) {
-            // Ask the human player if they want to roll 1 die or 2 dice
-            char choice;
+    if (canThrowOneDie() && isHuman) {
+        // Ask the human player if they want to roll 1 die or 2 dice
+        do {
+            std::cout << "Do you want to roll 1 die or 2 dice? (1/2): ";
+            std::cin >> choice;
+        } while (choice != '1' && choice != '2');
+    } else {
+        // Computer or must roll two dice
+        choice = '2';
+    }
+
+    // Ask if the player wants to enter the die manually
+    do {
+        std::cout << "Do you want to enter the die manually? (y/n): ";
+        std::cin >> isManual;
+    } while (isManual != 'y' && isManual != 'n');
+
+    if (isManual == 'y') {
+        if (choice == '1') {
+            // Validate input for 1 die (must be between 1 and 6)
             do {
-                cout << "Do you want to roll 1 die or 2 dice? (1/2): ";
-                cin >> choice;
-            } while (choice != '1' && choice != '2');
-
-            if (choice == '1') {
-                cout << "[1 die] ";
-                diceSum = rand() % 6 + 1; // Roll one die (1-6)
-            } else {
-                cout << "[2 dice] ";
-                diceSum = (rand() % 6 + 1) + (rand() % 6 + 1); // Roll two dice (2-12)
-            }
+                std::cout << "Enter the value for 1 die (1-6): ";
+                std::cin >> choiceOne;
+            } while (choiceOne < 1 || choiceOne > 6);
+            diceSum = choiceOne;
         } else {
-            // Computer chooses to roll 1 die or 2 dice randomly
-            if (rand() % 2 == 0) {
-                cout << "[1 die] ";
-                diceSum = rand() % 6 + 1; // Roll one die (1-6)
-            } else {
-                cout << "[2 dice] ";
-                diceSum = (rand() % 6 + 1) + (rand() % 6 + 1); // Roll two dice (2-12)
-            }
+            // Validate input for 2 dice (each die must be between 1 and 6)
+            do {
+                std::cout << "Enter the values for 2 dice (each 1-6): ";
+                std::cin >> choiceOne >> choiceTwo;
+            } while (choiceOne < 1 || choiceOne > 6 || choiceTwo < 1 || choiceTwo > 6);
+            diceSum = choiceOne + choiceTwo;
         }
     } else {
-        // Must roll two dice if any square from 7 to n is uncovered
-        cout << "[2 dice] ";
-        diceSum = (rand() % 6 + 1) + (rand() % 6 + 1); // Roll two dice (2-12)
+        if (choice == '1') {
+            diceSum = rand() % 6 + 1; // Roll one die (1-6)
+        } else {
+            diceSum = (rand() % 6 + 1) + (rand() % 6 + 1); // Roll two dice (2-12)
+        }
     }
 
     return diceSum;
