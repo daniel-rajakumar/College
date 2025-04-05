f38d2def2cd526b8197ce4337ad082b9a19dd6da
Daniel Rajakumar <danielelvisr@gmail.com>
2025-04-05 17:04:18 -0400

    feat: rename menu to end; update related classes and XML layouts

---
+++ OPL/Android/app/src/main/java/com/example/canoga/EndViewModel.java
@@ -0,0 +1,7 @@
+package com.example.canoga;
+
+import androidx.lifecycle.ViewModel;
+
+public class EndViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
--- OPL/Android/app/src/main/java/com/example/canoga/end.java
+++ OPL/Android/app/src/main/java/com/example/canoga/end.java
@@ -12,24 +12,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class menu extends Fragment {
+public class end extends Fragment {
 
-    private MenuViewModel mViewModel;
+    private EndViewModel mViewModel;
 
-    public static menu newInstance() {
-        return new menu();
+    public static end newInstance() {
+        return new end();
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        return inflater.inflate(R.layout.fragment_end, container, false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(MenuViewModel.class);
+        mViewModel = new ViewModelProvider(this).get(EndViewModel.class);
         // TODO: Use the ViewModel
     }
 
